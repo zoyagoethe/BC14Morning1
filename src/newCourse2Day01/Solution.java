f244fd89f4e83941ca4e2741a43a994b8cfe7a30
Zoya Göthe <zoya.goethe13@gmail.com>
2020-11-23 20:40:28 +0100

    homework 01 done

--- src/newCourse2Day01/Solution.java
+++ src/newCourse2Day01/Solution.java
@@ -2,26 +2,32 @@ package newCourseDay12;
 
 public class Solution {
     public static void main(String[] args) {
+        split();
         System.out.println(stringReverse("hello"));
         System.out.println(stringReverse("Dummkopf"));
-        //System.out.println(squareRecursive(5));
+        split();
+        System.out.println(squareRecursive(5));
+        System.out.println(squareRecursive(3498));
+        split ();
 
     }
 
-    /*private static int squareRecursive(int n) {
-        if (n == 0) {
-            return 0;
-        } else if (n == 1) {
+    private static void split() {
+        System.out.println("**********");
+    }
+
+    private static int squareRecursive(int n) {
+        if (n == 1) {
             return 1;
         } else {
-            return ???;
+            return squareRecursive(n-1)+n + n - 1;
         }
-    }*/
+    }
 
     private static String stringReverse(String str) {
         String output = "";
         for (int i = str.length() - 1; i >= 0; i--) {
-            output = output + str.charAt(i);
+            output += str.charAt(i);
         }
         return output;
     }
---
+++ src/newCourse2Day01/hw01/HomeWork01.java
@@ -0,0 +1,38 @@
+package newCourse2Day01.hw01;
+
+public class HomeWork01 {
+
+    public HomeWork01() {
+    }
+
+    public static int nod(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return nod(b, a % b);
+    }
+
+    public static int squareRecursive(int n){
+        if (n == 1) {
+            return 1;
+        } else {
+            return squareRecursive(n - 1) + n + n - 1;
+        }
+    }
+
+    public static String reverse(String str){
+        char[] output = new char[str.length()];
+        for (int i = 0; i < output.length; i++) {
+            output[i] = str.charAt(str.length() - 1 - i);
+        }
+        return new String(output);
+    }
+
+    static class HomeWork01Test{
+        public static void main(String[] args) {
+            System.out.println(nod(338, 4523));
+            //System.out.println(squareRecursive(356));
+            //System.out.println(reverse("KarabasBarabaS"));
+        }
+    }
+}
